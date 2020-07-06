3e63c576de09772a444301d5aac29620f8ed7286
senaszel <senaszel@gmail.com>
2020-07-06 02:18:33 +0200

    cosmetics and will be finished

---
+++ BusDriver.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace SD_29_BusDriversGossips
+{
+    class BusDriver
+    {
+        private List<int> route;
+
+        public bool[] KnownGossips { get; set; }
+        public List<int> Route { get; set; }
+
+
+        public BusDriver(List<int> _route)
+        {
+            Route = new List<int>();
+            do
+            {
+                Route.AddRange(_route);
+
+            } while (Route.Count < 420);
+            Route = Route.Take(420).ToList();
+        }
+
+
+        public override string ToString()
+        {
+            string route = String.Join(',', Route);
+            string gossips = String.Join(',', KnownGossips);
+
+
+            return $"This bus goes:\n{route}\nand knows:\n{gossips}";
+        }
+
+
+    }
+}
--- Program.cs
+++ Program.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Reflection.Metadata.Ecma335;
 
 namespace SD_29_BusDriversGossips
 {
@@ -8,110 +9,168 @@ namespace SD_29_BusDriversGossips
     {
         static void Main(string[] args)
         {
+            int nbOfDrivers = AskHowManyDrivers();
+            List<BusDriver> drivers = SetRoutesForEm(nbOfDrivers);
+            Introduction();
+            IntroduceDriversToUser(drivers);
+            (int minutes, List<BusDriver> drivers) answer = SimulateShift(drivers);
+            GiveAnswer(answer.minutes);
+            IntroduceDriversToUser(answer.drivers);
+        }
+
+        private static void Introduction()
+        {
+            Console.Clear(); Console.WriteLine("\tAt the beginning all drivers know only one gossip: ");
+            Console.WriteLine("And pick ups more while encountering other drivers at the same bus stops. They share gossips until...");
+        }
+
+        private static void IntroduceDriversToUser(List<BusDriver> drivers)
+        {
+            drivers.ForEach(x => { Console.WriteLine(); Console.WriteLine(x.ToString()); });
+        }
+
+        private static void GiveAnswer(int answer)
+        {
+            if (answer > -1)
+                Console.WriteLine($"\n\n\tEvery driver already know every gossip just after passing {answer+1} stops.\n");
+            else
+                Console.WriteLine("\n\n\tNEVER");
+        }
+
+
+        private static (int, List<BusDriver>) SimulateShift(List<BusDriver> drivers)
+        {
+            List<int> stops = new List<int>();
+
+            for (int i = 0; i < 420; i++)
+            {
+                Console.WriteLine("!!!!!!!!!!!!!!!!!!!!!!!!!!" + i + "!!!!!!!!!!!!!!!!!!!!!!!!!!!!");
+
+                stops.Clear();
+
+                drivers.ForEach(x =>
+                {
+                    stops.Add(x.Route[i]);
+                });
+
+                if (i == 0)
+                {
+                    drivers.ForEach(x =>
+                    {
+                        Console.WriteLine("count="+x.Route.Count);
+
+                    });
+                }
+
+                for (int oneDriver = 0; oneDriver < stops.Count; oneDriver++)
+                {
+                    for (int anotherDriver = 0; anotherDriver < stops.Count; anotherDriver++)
+                    {
+                        if (oneDriver != anotherDriver)
+                        {
+                            if (stops[oneDriver] == stops[anotherDriver])
+                            {
+                                ShareGossip(drivers, oneDriver, anotherDriver);
+
+                                //IntroduceDriversToUser(drivers);
+
+                                bool allKnowEverything = true;
+                                for (int o = 0; o < drivers.Count; o++)
+                                {
+                                    for (int p = 0; p < drivers[o].KnownGossips.Length; p++)
+                                    {
+                                        if (drivers[o].KnownGossips[p] == false)
+                                        {
+                                            allKnowEverything = false;
+                                        }
+                                    }
+
+                                }
+
+                                if (allKnowEverything)
+                                {
+                                    return (i, drivers);
+                                }
+                                else
+                                    Console.WriteLine($"driver {oneDriver} meets driver {anotherDriver} on stops nb:{stops[oneDriver]}");
+                            }
+
+                        }
+                    }
+                }
+            }
+            return (-1, drivers);
+        }
+
+        private static void ShareGossip(List<BusDriver> drivers, int one, int another)
+        {
+            for (int m = 0; m < drivers[one].KnownGossips.Length; m++)
+            {
+                if (drivers[one].KnownGossips[m] == false && drivers[another].KnownGossips[m] == true)
+                {
+                    drivers[one].KnownGossips[m] = true;
+                }
+
+                if (drivers[another].KnownGossips[m] == false && drivers[one].KnownGossips[m] == true)
+                {
+                    drivers[another].KnownGossips[m] = true;
+                }
+            }
+        }
+
+        private static List<BusDriver> SetRoutesForEm(int nbOfDrivers)
+        {
+            List<BusDriver> drivers = new List<BusDriver>();
+            for (int i = 0; i < nbOfDrivers; i++)
+            {
+                List<int> intted = new List<int>();
+                string[] splitted;
+                do
+                {
+                    Console.Clear();
+                    Console.WriteLine(i + "Put stops numbers, divided by space, in a sequence the bus will be passing:");
+
+                    intted.Clear();
+                    string _userInput = Console.ReadLine();
+                    splitted = _userInput.Split(' ', StringSplitOptions.RemoveEmptyEntries);
+                    for (int j = 0; j < splitted.Length; j++)
+                    {
+                        if (int.TryParse(splitted[j], out int _temp))
+                            intted.Add(_temp);
+                    }
+                } while (intted.Count != splitted.Length);
+
+                BusDriver busDriver = new BusDriver(intted);
+                drivers.Add(busDriver);
+            }
+
+            for (int i = 0; i < drivers.Count; i++)
+            {
+                drivers[i].KnownGossips = Enumerable.Repeat(false, drivers.Count).ToArray();
+                drivers[i].KnownGossips[i] = true;
+            }
+
+            return drivers;
+        }
+
+
+        private static int AskHowManyDrivers()
+        {
             int nbOfDrivers;
             do
             {
-                Console.WriteLine("How many bus drivers hit the road this time?");
-                int.TryParse(Console.ReadLine(), out int _temp);
-                nbOfDrivers = _temp;
-            } while (nbOfDrivers <= 0);
+                Console.Clear();
+                Console.WriteLine("How many drivers hit the road?");
 
-            List<int> allStops = new List<int>();
-            List<List<int>> AllBusDrivers = new List<List<int>>();
-            for (int i = 0; i < nbOfDrivers; i++)
-            {
-                AllBusDrivers.Add(GetRoute(ref allStops));
-            }
+                bool parsed = int.TryParse(Console.ReadLine(), out int _temp);
+                nbOfDrivers = parsed == true ? _temp : 0;
 
-            List<List<int>> beenWhere = new List<List<int>>(420);
-            for (int i = 0; i < 420; i++)
-            {
-                beenWhere.Add(new List<int>());
-            }
-            AllBusDrivers.ForEach(driver => Simulate_8hours(driver, beenWhere));
-
-            //
-            // Dobra masz generowanie tras
-            // i miejsca gdzie sa ktorzy o jakiej porze
-            // no to teraz 
-            // odpierdol wykreslanke kto co wie i 
-            // skreslanie jak sie spotykaja
-            //
-
-            int[,] tableOfKnowledge = new int[AllBusDrivers.Count, AllBusDrivers.Count];
-            for (int i = 0; i < tableOfKnowledge.GetLength(0); i++)
-            {
-                for (int j = 0; j < tableOfKnowledge.GetLength(1); j++)
-                {
-                    if (i == j)
-                        tableOfKnowledge[i, j] = 1;
-                }
-            }
-
-            for (int i = 0; i < tableOfKnowledge.GetLength(0); i++)
-            {
-                for (int j = 0; j < tableOfKnowledge.GetLength(1); j++)
-                {
-                    Console.Write("[" + tableOfKnowledge[i, j] + "]");
-                }
-                Console.WriteLine();
-            }
-
-            Console.ReadKey();
-
-            Console.WriteLine(
-            AllBusDrivers.Count()
-                );
-            for (int i = 0; i < AllBusDrivers.Count(); i++)
-            {
-                AllBusDrivers[i].ForEach(x => Console.Write(x));
-                Console.WriteLine();
-            }
+            } while (nbOfDrivers <= 1);
 
 
-            Console.WriteLine("\n\n\t================================theEnd");
+            return nbOfDrivers;
         }
 
-        private static List<int> GetRoute(ref List<int> allStops)
-        {
-            Console.WriteLine("\n\tPut stops numbers describing route of this particullar driver (one full circle)");
-            List<int> stops;
-            do
-            {
-                var _temp = Console.ReadLine().Split(' ', StringSplitOptions.RemoveEmptyEntries);
-                stops = _temp.ToList().ConvertAll(stop => Convert.ToInt32(stop));
-
-            } while (stops.Count <= 0);
-            allStops = allStops.Union(stops).ToList();
-            Console.WriteLine();
-
-            return stops;
-        }
-
-        private static void Simulate_8hours(List<int> stops, List<List<int>> data)
-        {
-            Console.WriteLine("\tThis one goes (minute by minute):");
-
-            List<List<int>>.Enumerator dataEnumerator = data.GetEnumerator();
-            List<int>.Enumerator stopEnumer = stops.GetEnumerator();
-            for (int i = 0; i < 420; i++)
-            {
-                if (stopEnumer.MoveNext())
-                {
-                    dataEnumerator.MoveNext();
-                    int _temp = stopEnumer.Current;
-                    dataEnumerator.Current.Add(_temp);
-                    Console.Write(_temp);
-                    Console.Write(" ");
-                }
-                else
-                {
-                    stopEnumer = stops.GetEnumerator();
-                    i--;
-                }
-            }
-            Console.WriteLine();
-        }
 
     } // end of class
 } // end of namespace
